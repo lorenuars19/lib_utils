22e578397bfe3b6405ba28db7ff9e08b837705a4
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-06-24 18:33:22 +0200

    remove debug

--- src/stack/stack_free.c
+++ src/stack/stack_free.c
@@ -6,14 +6,12 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 20:32:46 by lorenuar          #+#    #+#             */
-/*   Updated: 2021/06/24 18:30:11 by lorenuar         ###   ########.fr       */
+/*   Updated: 2021/06/24 18:33:03 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "lib_stack.h"
-
-# include <debug_utils.h>
 
 /*
 ** Free the whole stack
@@ -23,7 +21,6 @@ void	stack_free(t_stack *stack)
 {
 	if (stack && stack->dat)
 	{
-DBM(stack_free, stack, stack->dat)
 		free(stack->dat);
 		stack->dat = NULL;
 		stack->siz = 0;
