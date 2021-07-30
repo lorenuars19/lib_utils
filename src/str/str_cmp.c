7c26d342dadf5d30009c79072ac10a9d8e4c0f7c
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-07-30 18:22:56 +0200

    unfix crash

--- src/str/str_cmp.c
+++ src/str/str_cmp.c
@@ -6,7 +6,7 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 17:26:41 by lorenuar          #+#    #+#             */
-/*   Updated: 2021/05/17 22:05:27 by lorenuar         ###   ########.fr       */
+/*   Updated: 2021/07/30 13:46:39 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ int	str_cmp(const char *s1, const char *s2)
 	size_t	i;
 
 	i = 0;
-	while (s1 && s1[i] && s2 && s2[i] && s1[i] == s2[i])
+	while (s1 && s2 && s1[i] && s2[i] && s1[i] == s2[i])
 	{
 		i++;
 	}
-	if (s1 && s1[i] && s2 && s2[i] && s1[i] == s2[i])
+	if (s1 && s2 && s1[i] && s2[i] && s1[i] == s2[i])
 	{
 		return (0);
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	if (s1 && s2)
+	{
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return ((unsigned char *)s1 - (unsigned char *)s2);
 }
 
 int	str_cmp_n(const char *s1, const char *s2, size_t size)
