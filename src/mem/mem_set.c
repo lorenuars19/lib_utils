8d6c9c77f2f420233a5705e03a40d8fe19563ada
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-08-07 17:35:54 +0200

    make code more readable

--- src/mem/mem_set.c
+++ src/mem/mem_set.c
@@ -6,18 +6,21 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/07 17:16:28 by lorenuar          #+#    #+#             */
-/*   Updated: 2021/08/07 17:34:42 by lorenuar         ###   ########.fr       */
+/*   Updated: 2021/08/07 17:35:24 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib_mem.h"
 
-void	*mem_set(void *ptr, int dat, long len)
+void	*mem_set(void *ptr, int dat, long size)
 {
 	t_mem	*tmp;
 
 	tmp = (t_mem *)ptr;
-	while (tmp && len > 0)
-		tmp[--len] = dat;
+	while (tmp && size >= 0)
+	{
+		tmp[size] = dat;
+		size++;
+	}
 	return (ptr);
 }
