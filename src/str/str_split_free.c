dd03508837f002961ca30607e644154311515bd1
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-08-08 15:02:07 +0200

    fix str_split_free

--- src/str/str_split_free.c
+++ src/str/str_split_free.c
@@ -6,22 +6,25 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 18:22:05 by lorenuar          #+#    #+#             */
-/*   Updated: 2021/08/07 23:43:41 by lorenuar         ###   ########.fr       */
+/*   Updated: 2021/08/08 14:54:02 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib_str.h"
 #include <stdlib.h>
 
-void	tab_free(char ***tab)
+void	tab_free(char ***input)
 {
-	int	i;
+	char	**tab;
+	int		i;
 
+	tab = *input;
 	i = 0;
-	while (tab && *tab && *tab[i])
+	while (tab && tab[i])
 	{
-		free(*tab[i]);
+		free(tab[i]);
+		i++;
 	}
-	free(*tab);
-	*tab = NULL;
+	free(*input);
+	*input = NULL;
 }
